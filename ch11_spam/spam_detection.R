68a2d0f7862d2f15f07f78ecfa2698db05e282cc
jakinpilla <jakinpilla@gmail.com>
2019-08-13 22:39:59 +0900

    updated

--- ch11_spam/spam_detection.R
+++ ch11_spam/spam_detection.R
@@ -1,4 +1,11 @@
 library(tidyverse)
+library(RMySQL)
+
+Packages <- c("MASS", "glmnet", "randomForest", "gbm", "rpart",
+              "boot", "data.table", "ROCR", "gridExtra")
+lapply(Packages, library, character.only = T)
+
+
 setwd("C:/Users/Daniel/Documents/ipds_kr_R_book/ch11_spam/")
 read.table("spambase.data", strip.white = T, sep = ",", header = F) %>% 
   as_tibble() -> data
@@ -20,7 +27,6 @@ names(data) <- c('word_freq_make', 'word_freq_address', 'word_freq_all', 'word_f
 
 data %>% View()
 
-library(RMySQL)
 con <- dbConnect(
   MySQL(),
   user = "root", 
@@ -40,6 +46,232 @@ con_spam <- dbConnect(
   dbname = "spambase"
 )
 
-data <- dbGetQuery(con_spam, "select * from spambase;")
+data_from_db <- dbGetQuery(con_spam, "select * from spambase;")
+data_from_db %>% colnames()
 
-data %>% glimpse()
+data_from_db %>% select(-row_names) -> data_spam
+data_spam %>% glimpse()
+
+data_spam %>% select(1:10, 58) %>% sample_n(500) -> data_tmp_1
+
+library(GGally)
+ggpairs(data_tmp_1)
+
+data_spam %>% select(48:57, 58) %>% sample_n(500) -> data_tmp_2
+ggpairs(data_tmp_2)
+
+cor(data_spam[, -58], as.numeric(data_spam$class)) %>% as.data.frame() %>%
+  rename(cor = V1) -> tmp
+
+tmp$var <- rownames(tmp)
+
+tmp %>%
+  ggplot(aes(reorder(var, cor), cor)) + 
+  geom_point() +
+  coord_flip()
+
+library(gridExtra)
+
+data_spam$class <- as.factor(data_spam$class)
+
+data_spam %>% ggplot(aes(class)) + geom_bar() -> p1; p1
+
+data_spam %>% ggplot(aes(class, `word_freq_$`)) + 
+  geom_jitter(col = 'gray') + geom_boxplot(alpha = .5) + scale_y_sqrt() -> p2; p2
+
+data_spam %>% ggplot(aes(`word_freq_$`, group=class, fill = class)) + 
+  geom_density(alpha = .5) + scale_x_sqrt() + scale_y_sqrt() -> p3; p3
+
+data_spam %>% ggplot(aes(class, capital_run_length_longest)) +
+  geom_jitter(col ='gray') +
+  geom_boxplot(alpha = .5) + 
+  scale_y_log10() -> p4; p4
+
+grid.arrange(p1, p2, p3, p4, ncol = 2)
+
+
+# 특수문자 변수명 처리 -------------------------------------------------------------
+
+library(randomForest)
+# data_rf <- randomForest(class ~. , data = data_spam)
+
+old_names <- names(data_spam)
+new_names <- make.names(old_names, unique = T)
+
+cbind(old_names, new_names)[old_names != new_names, ]
+
+names(data_spam) <- new_names
+
+
+# Data Spliting -----------------------------------------------------------
+nrow(data_spam) -> n
+1:n %>% sample(., n*.6) -> train_idx
+setdiff(1:n, train_idx) -> idx_without_train
+idx_without_train %>% sample(., n*.2) -> validation_idx
+setdiff(idx_without_train, validation_idx) -> test_idx
+
+train_idx %>% length()
+validation_idx %>% length()
+test_idx %>% length()
+
+
+train_data <- data_spam[train_idx, ]
+validation_data <- data_spam[validation_idx, ]
+test_data <- data_spam[test_idx, ]
+
+
+# Logistic Regression -----------------------------------------------------
+
+lm_full <- glm(class ~., data = train_data, family = binomial)
+summary(lm_full)
+
+
+# Validation --------------------------------------------------------------
+library(ROCR)
+y_obs <- as.numeric(as.character(validation_data$class))
+yhat_lm <- predict(lm_full, newdata = validation_data, type = 'response')
+pred_lm <- prediction(yhat_lm, y_obs)
+performance(pred_lm, "auc")@y.values[[1]]
+
+binomial_deviance <- function(y_obs, yhat) {
+  epsilon = 0.0001
+  yhat = ifelse(yhat < epsilon, epsilon, yhat)
+  yhat = ifelse(yhat > 1- epsilon, 1-epsilon, yhat)
+  a = ifelse(y_obs == 0, 0, y_obs * log(y_obs/yhat))
+  b = ifelse(y_obs==1, 0, (1-y_obs) * log((1-y_obs)/(1-yhat)))
+  return((2*sum(a+b)))
+}
+
+binomial_deviance(y_obs, yhat_lm)
+
+
+# Lasso -------------------------------------------------------------------
+
+xx <- model.matrix(class ~.-1, data_spam)
+x <- xx[train_idx, ]
+y <- as.numeric(as.character(train_data$class))
+glimpse(x)
+
+data_cvfit <- cv.glmnet(x, y, family = "binomial")
+plot(data_cvfit)
+
+coef(data_cvfit, s = c("lambda.1se"))
+coef(data_cvfit, s = c("lambda.min"))
+
+predict.cv.glmnet(data_cvfit, s = "lambda.min", newx = x[1:5, ], type = "response")
+
+yhat_glmnet <- predict(data_cvfit, s= "lambda.min", newx = xx[validation_idx, ], 
+                       type = 'response')
+
+yhat_glmnet <- yhat_glmnet[, 1] # change to a vector from [n*1] matrix....
+
+pred_glmnet <- prediction(yhat_glmnet, y_obs)
+performance(pred_glmnet, "auc")@y.values[[1]]
+binomial_deviance(y_obs, yhat_glmnet)
+
+# Tree Model ---------------------------------------------------------------
+
+data_tr <- rpart(class ~., data = train_data)
+data_tr
+
+printcp(data_tr)
+summary(data_tr)
+
+opar <- par(mfrow  = c(1, 1), xpd=NA)
+plot(data_tr)
+text(data_tr, use.n = T)
+par(opar)
+
+yhat_tr <- predict(data_tr, validation_data)
+yhat_tr <- yhat_tr[, "1"]
+pred_tr <- prediction(yhat_tr, y_obs)
+performance(pred_tr, "auc")@y.values[[1]]
+binomial_deviance(y_obs, yhat_tr)
+
+# RandomForest ------------------------------------------------------------
+
+set.seed(2019)
+data_rf <- randomForest(class ~., train_data)
+data_rf
+
+opar <- par(mfrow = c(1, 2))
+plot(data_rf)
+varImpPlot(data_rf)
+par(opar)
+
+yhat_rf <- predict(data_rf, newdata = validation_data, type = 'prob')[, '1']
+pred_rf <- prediction(yhat_rf, y_obs)
+performance(pred_rf, "auc")@y.values[[1]]
+binomial_deviance(y_obs, yhat_rf)
+
+
+# Boosting ----------------------------------------------------------------
+
+set.seed(2019)
+data_for_gbm <- train_data %>%
+  mutate(class = as.numeric(as.character(class)))
+
+data_gbm <- gbm(class ~., data = data_for_gbm, distribution = "bernoulli", 
+                n.trees=100000, cv.folds=3, verbose=T)
+
+(best_iter= gbm.perf(data_gbm, method="cv"))
+
+yhat_gbm <- predict(data_gbm, n.trees=best_iter, newdata=validation_data, type='response')
+pred_gbm <- prediction(yhat_gbm, y_obs)
+performance(pred_gbm, "auc")@y.values[[1]]
+binomial_deviance(y_obs, yhat_gbm)
+
+method = c('lm', 'glmnet', 'rf', 'gbm')
+auc = c(performance(pred_glmnet, "auc")@y.values[[1]], 
+        performance(pred_tr, "auc")@y.values[[1]],
+        performance(pred_rf, "auc")@y.values[[1]], 
+        performance(pred_gbm, "auc")@y.values[[1]])
+
+bin_dev = c(binomial_deviance(y_obs, yhat_glmnet),
+            binomial_deviance(y_obs, yhat_tr),
+            binomial_deviance(y_obs, yhat_rf),
+            binomial_deviance(y_obs, yhat_gbm))
+
+
+data.frame(method = method, 
+           auc = auc,
+           bin_dev = bin_dev)
+
+perf_lm <- performance(pred_lm, measure = "tpr", x.measure = "fpr")
+perf_glmnet <- performance(pred_glmnet, measure = "tpr", x.measure = "fpr")
+perf_rf <- performance(pred_rf, measure="tpr", x.measure = "fpr")
+perf_gbm <- performance(pred_gbm, measure = "tpr", x.measure = "fpr")
+
+plot(perf_lm, col="black", main="ROC Curve")
+plot(perf_glmnet, add=T, col="blue")
+plot(perf_rf, add=T, col="red")
+plot(perf_gbm, add=T, col="cyan")
+abline(0, 1)
+legend('bottomright', insert.1, 
+       legend=c("GLM", "glmnet", "RF", "GBM"), 
+       col = c('black', 'blue', 'red', 'cyan'), lty=1, lwd=2)
+
+# rf is winner...
+# we apply test data to randomforest model...
+
+y_obs_test <- test_data$class %>% as.character() %>% as.numeric()
+yhat_rf_test <- predict(data_rf, newdata=test_data, type='prob')[, '1']
+pred_rf <- prediction(yhat_rf_test, y_obs_test)
+
+
+performance(pred_rf, "auc")@y.values[[1]]
+binomial_deviance(y_obs_test, yhat_rf_test)
+
+# Visualization
+data.frame(y_obs=y_obs,
+                 yhat_lm = yhat_lm,
+                 yhat_glmnet = yhat_glmnet, 
+                 yhat_rf = yhat_rf,
+                 yhat_gbm = yhat_gbm) %>%
+  ggpairs()
+
+
+
+
+
+
---
+++ ch13_house_price_regression/house_price_regression.R
@@ -0,0 +1,199 @@
+setwd("~/ipds_kr_R_book/ch13_house_price_regression")
+read.table("housing.data", strip.white=T) %>% as_tibble() -> data_house
+names(data_house) <- c('crim', 'zn', 'indus', 'chas', 'nox', 'rm', 'age', 'dis', 
+                 'rad', 'tax', 'ptratio', 'b', 'lstat', 'medv')
+glimpse(data_house)
+summary(data_house)
+
+
+# -------------------------------------------------------------------------
+
+con <- dbConnect(
+  MySQL(),
+  user = "root", 
+  password = "chr0n3!7!"
+)
+
+dbSendQuery(con, "CREATE DATABASE house_price;")
+dbSendQuery(con, "USE house_price;")
+
+dbWriteTable(con, "data_house", data, overwrite = T)
+dbDisconnect(con)
+
+con_house_price <- dbConnect(
+  MySQL(),
+  user = "root", 
+  password = "chr0n3!7!",
+  dbname = "house_price"
+)
+
+data_from_db <- dbGetQuery(con_house_price, "select * from data_house;")
+data_from_db %>% colnames()
+
+data_from_db %>% select(-row_names) -> data_house
+data_house %>% glimpse()
+
+
+# -------------------------------------------------------------------------
+
+data_house %>% ggpairs() 
+
+set.seed(2019)
+nrow(data_house) -> n
+1:n %>% sample(., n*.6) -> train_idx
+setdiff(1:n, train_idx) -> idx_without_train
+idx_without_train %>% sample(., n*.2) -> validate_idx
+setdiff(idx_without_train, validation_idx) -> test_idx
+
+train_idx %>% length()
+validate_idx %>% length()
+test_idx %>% length()
+
+
+train_data <- data_house[train_idx, ]
+validate_data <- data_house[validate_idx, ]
+test_data <- data_house[test_idx, ]
+
+data_lm_full <- lm(medv ~., data=train_data)
+summary(data_lm_full)
+
+predict(data_lm_full, newdata=data_house[1:5, ])
+
+plot(data_lm_full)
+
+data_lm_full_2 <- lm(medv ~.^2, data=train_data)
+summary(data_lm_full_2)
+
+length(coef(data_lm_full_2))
+
+library(MASS)
+data_step <- step(data_lm_full, scope=list(upper = ~.^2, lower= ~1))
+data_step
+anova(data_step)
+summary(data_step)
+length(coef(data_step))
+
+
+# Validation --------------------------------------------------------------
+
+y_obs <- validate_data$medv
+yhat_lm <- predict(data_lm_full, newdata=validate_data)
+yhat_lm_2 <- predict(data_lm_full_2, newdata=validate_data)
+yhat_step <- predict(data_step, newdata=validate_data)
+
+library(modelr)
+rmse(data_lm_full, validate_data)
+rmse(data_lm_full_2, validate_data)
+rmse(data_step, validate_data)
+
+
+# Lasso -------------------------------------------------------------------
+
+xx <- model.matrix(medv ~. ^2-1, data)
+x <- xx[train_idx, ]
+y <- train_data$medv
+
+glimpse(x)
+
+data_cvfit <- cv.glmnet(x, y)
+plot(data_cvfit)
+
+coef(data_cvfit, s = c("lambda.1se"))
+coef(data_cvfit, s = c("lambda.min"))
+
+
+# Elastic -----------------------------------------------------------------
+
+data_cvfit_es <- cv.glmnet(x, y, alpha=.5)
+plot(data_cvfit_es)
+
+coef(data_cvfit_es, s = c("lambda.1se"))
+coef(data_cvfit_es, s = c("lambda.min"))
+
+predict.cv.glmnet(data_cvfit, s = "lambda.min", newx = x[1:5, ])
+
+y_obs <- validate_data$medv
+yhat_glmnet <- predict(data_cvfit, s = "lambda.min", newx=xx[validate_idx, ])
+yhat_glmnet <- yhat_glmnet[, 1]
+
+data.frame(y_obs, yhat_glmnet) %>%
+  as_tibble() %>%
+  mutate(residue = y_obs - yhat_glmnet) %>%
+  mutate(sq_residue  = residue^2) %>%
+  summarise(mean.sum.sq_residue = sum(sq_residue)/nrow(.)) %>%
+  mutate(rmse = sqrt(mean.sum.sq_residue)) -> df.rmse_glmnet
+
+df.rmse_glmnet$rmse
+
+# Tree for regression -----------------------------------------------------
+
+data_tr <- rpart(medv ~., data=train_data)
+yhat_tr <- predict(data_tr, newdata = validate_data)
+
+rmse(data_tr, validate_data)
+
+
+# RandomForest ------------------------------------------------------------
+
+set.seed(2019)
+data_rf <- randomForest(medv ~., train_data)
+data_rf
+
+plot(data_rf)
+varImpPlot(data_rf)
+
+yhat_rf <- predict(data_rf, newdata = validate_data)
+
+rmse(data_rf, validate_data)
+
+
+# Boosting ----------------------------------------------------------------
+
+set.seed(2019)
+data_gbm <- gbm(medv ~., data=train_data, 
+                n.trees=40000, cv.folds = 3, 
+                verbose = T)
+(best_iter = gbm.perf(data_gbm, method="cv"))
+
+yhat_gbm <- predict(data_gbm, n.trees=best_iter, newdata=validate_data)
+rmse(data_gbm, validate_data)
+
+
+# metirc ------------------------------------------------------------------
+
+data.frame(
+  method = c("lm", "glm", "dt", "rf", "gbm"),
+  rmse = c(rmse(data_step, validate_data),
+           df.rmse_glmnet$rmse,
+           rmse(data_tr, validate_data),
+           rmse(data_rf, validate_data), 
+           rmse(data_gbm, validate_data))
+  )
+)
+
+
+# Residue Boxplot....
+y_obs
+yhat_step
+yhat_glmnet
+yhat_tr
+yhat_rf
+yhat_gbm
+
+residue_lm <- y_obs - yhat_step
+residue_glmnet <- y_obs  - yhat_glmnet
+residue_tr <- y_obs - yhat_tr
+residue_rf <- y_obs - yhat_rf
+residue_gbm <- y_obs - yhat_gbm
+
+data.frame(residue_lm, residue_glmnet, residue_tr, residue_rf, residue_gbm) %>% 
+  as_tibble() %>%
+  gather() %>% 
+  ggplot(aes(key, value, col=key)) + geom_boxplot(alpha=.5) + geom_jitter()
+
+
+
+
+
+
+
